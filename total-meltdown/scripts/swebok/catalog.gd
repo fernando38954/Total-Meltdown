8a4a24520ad696125b2edf4300eab6233c5745e4
Hagisf <fernando38954@gmail.com>
2026-05-18 18:33:02 -0300

    Import Notebook Markers sprite

--- total-meltdown/scripts/swebok/catalog.gd
+++ total-meltdown/scripts/swebok/catalog.gd
@@ -1,10 +1,9 @@
 extends Control
 
-@export var button_entry_scene: PackedScene
 @export var swebok: Swebok = null
 
 @export_category("Container")
-@export var tutorial_container: Container
+@export var attribute_container: Container
 @export var pattern_container: Container
 
 func _ready():
@@ -13,22 +12,7 @@ func _ready():
 	build_catalog()
 
 func build_catalog():
-	for child in tutorial_container.get_children() + pattern_container.get_children():
-		child.queue_free()
-	
-	for idx in range(TutorialPageManager.all_pages.size()):
-		var page = TutorialPageManager.all_pages[idx]
-		var button = button_entry_scene.instantiate()
-		button.text = page.title
-		button.pressed.connect(_on_page_selected.bind(idx, ContentViewer.ContentType.Tutorial))
-		tutorial_container.add_child(button)
-	
-	for idx in range(PatternManager.owned_patterns.size()):
-		var pattern = PatternManager.owned_patterns[idx]
-		var button = button_entry_scene.instantiate()
-		button.text = pattern.title
-		button.pressed.connect(_on_page_selected.bind(idx, ContentViewer.ContentType.Pattern))
-		pattern_container.add_child(button)
+	pass
 
 func _on_page_selected(index: int, content_type: ContentViewer.ContentType):
 	swebok.turn_to_page(index, content_type)
