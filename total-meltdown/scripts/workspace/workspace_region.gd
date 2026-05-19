5f2beba1881dd9d48dc8da7cdc45f5edbda3616b
Hagisf <fernando38954@gmail.com>
2026-05-19 19:07:53 -0300

    Add indicator to clickable areas

--- total-meltdown/scripts/workspace/workspace_region.gd
+++ total-meltdown/scripts/workspace/workspace_region.gd
@@ -9,7 +9,19 @@ class_name Region
 @export var focus: Marker2D   # posição global predeterminada do zoom
 @export var zoom_level: float = 2.0
 @export var duration: float = 0.3
+@export var indicator: Sprite2D = null
+
+func _ready() -> void:
+	indicator.hide()
 
 func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		workspace.switch_region(self)
+		indicator.hide()
+
+func _on_mouse_entered() -> void:
+	if workspace.current_region == null:
+		indicator.show()
+
+func _on_mouse_exited() -> void:
+	indicator.hide()
