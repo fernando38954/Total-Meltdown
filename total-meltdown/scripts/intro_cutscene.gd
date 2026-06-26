a80823e6badc952c906bb8a6521a8857dcb4cb21
Hagisf <fernando38954@gmail.com>
2026-06-26 20:02:34 -0300

    Update Intro Cutscene

--- total-meltdown/scripts/intro_cutscene.gd
+++ total-meltdown/scripts/intro_cutscene.gd
@@ -1,31 +1,40 @@
 extends Node2D
 
 const PHRASES_JSON_PATH := "cutscene/intro.json"
-@onready var sprites: Array[Sprite2D] = [
-	$FirstSet/BackgroundCity,
-	$FirstSet/CPUs,
-	$FirstSet/CodeRunning,
-	$FirstSet/ComputerError,
-	$FirstSet/BuildingFire,
-	$SecondSet/RetroCode,
-	$SecondSet/ComputerCalls,
-	$SecondSet/Agent
-]
 
-@onready var typing_label: AutoTypingRichTextLabel = $AutoTypingRichTextLabel
+@export var node_sets: Array[Node] = []
+var sprites: Array[Sprite2D] = []
+var labels: Array[AutoTypingRichTextLabel] = []
+var first_set_sprite_counter = 0
+var first_set_label_counter = 0
 
-var phrases: Array[String] = []
-var current_step := -1
+var frames_data: Array[Array] = []
+var current_frame_idx: int = -1
+var current_phrase_idx: int = 0
+var current_label_idx: int = -1
 var first_set_hided: bool = false
 var first_set_hiding: bool = false
 var switching_scene: bool = false
 var tween: Tween
 
 func _ready() -> void:
+	load_nodes()
 	load_phrases()
 	hide_all_sprites()
 	await get_tree().create_timer(1).timeout
 	advance_to_next()
+
+func load_nodes() -> void:
+	for node_set in node_sets:
+		for child in node_set.get_children():
+			if child is Sprite2D:
+				sprites.append(child)
+				if node_set.name == "FirstSet":
+					first_set_sprite_counter += 1
+			elif child is AutoTypingRichTextLabel:
+				labels.append(child)
+				if node_set.name == "FirstSet":
+					first_set_label_counter += 1
 
 func load_phrases() -> void:
 	var file_path = GlobalResource.get_current_content_path() + PHRASES_JSON_PATH
@@ -33,39 +42,59 @@ func load_phrases() -> void:
 	var json_string = file.get_as_text()
 	var json = JSON.new()
 	var error = json.parse(json_string)
-	if error == OK:
-		var data = json.data
-		for phrase in data.values():
+	if error != OK:
+		push_error("Error JSON parse: ", error)
+		return
+	
+	var data = json.data
+	for frame_data in data.values():
+		var phrases: Array[String] = []
+		for phrase in frame_data.values():
 			phrases.append(phrase)
+		frames_data.append(phrases)
 
 func hide_all_sprites() -> void:
 	for sprite in sprites:
 		sprite.modulate.a = 0
 
+func hide_all_labels() -> void:
+	for label in labels:
+		label.modulate.a = 0
+
 func animated_hiding_first_set() -> void:
 	first_set_hiding = true
 	tween = create_tween().set_parallel()
-	tween.tween_property(typing_label, "modulate:a", 0, 0.5)
-	for idx in range(5):
+	for idx in range(first_set_sprite_counter):
 		tween.tween_property(sprites[idx], "modulate:a", 0, 0.5)
+	for idx in range(first_set_label_counter):
+		tween.tween_property(labels[idx], "modulate:a", 0, 0.5)
 	await tween.finished
 	first_set_hided = true
 	first_set_hiding = false
 	advance_to_next()
 
 func advance_to_next() -> void:
-	typing_label.modulate.a = 1
-	if current_step < sprites.size() - 1:
-		current_step += 1
-		tween = create_tween()
-		tween.tween_property(sprites[current_step], "modulate:a", 1, 0.5)
-		typing_label.start_typing(phrases[current_step])
+	if current_frame_idx >= 0 and current_phrase_idx < frames_data[current_frame_idx].size() - 1:
+		current_phrase_idx += 1
+		current_label_idx += 1
+		labels[current_label_idx].modulate.a = 1
+		labels[current_label_idx].start_typing(frames_data[current_frame_idx][current_phrase_idx])
 	else:
-		switching_scene = true
-		await Fade.fade_out().finished  
-		get_tree().change_scene_to_file("res://scenes/GameScene.tscn")
-		GlobalSignal.emit_signal("game_start")
-		Fade.fade_in()
+		if current_frame_idx < sprites.size() - 1:
+			current_frame_idx += 1
+			tween = create_tween()
+			tween.tween_property(sprites[current_frame_idx], "modulate:a", 1, 0.5)
+			
+			current_phrase_idx = 0
+			current_label_idx += 1
+			labels[current_label_idx].modulate.a = 1
+			labels[current_label_idx].start_typing(frames_data[current_frame_idx][current_phrase_idx])
+		else:
+			switching_scene = true
+			await Fade.fade_out().finished  
+			get_tree().change_scene_to_file("res://scenes/GameScene.tscn")
+			GlobalSignal.emit_signal("game_start")
+			Fade.fade_in()
 
 #region Click Handle
 func _input(event: InputEvent) -> void:
@@ -75,17 +104,17 @@ func _input(event: InputEvent) -> void:
 func handle_click() -> void:
 	if switching_scene:
 		return
-	elif typing_label.is_typing:
+	elif labels[current_label_idx].is_typing:
 		if tween and tween.is_running():
 			tween.kill()
-		sprites[current_step].modulate.a = 1
-		typing_label.skip_typing()
-	elif current_step >= 4 and not first_set_hided:
+		sprites[current_frame_idx].modulate.a = 1
+		labels[current_label_idx].skip_typing()
+	elif current_frame_idx >= 4 and not current_phrase_idx < frames_data[current_frame_idx].size() - 1 and not first_set_hided:
 		if first_set_hiding:
 			if tween and tween.is_running():
 				tween.kill()
 			hide_all_sprites()
-			typing_label.modulate.a = 0
+			hide_all_labels()
 			first_set_hided = true
 			first_set_hiding = false
 			advance_to_next()
